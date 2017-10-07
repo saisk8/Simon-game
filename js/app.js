3555391d07482fb2b0d24cdddfcc36dfad9b2895
Sai Somanath.K <k.saisomanath@gmail.com>
2017-10-07 20:11:41 +0530

    Add app.js skeleton

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document)
+	.ready(function() {
+		var strict = false;
+		var level = 0;
+		var buttons = [0, 1, 2, 3];
+		var sounds = {
+			red: 'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',
+			blue: 'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
+			green: 'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
+			yellow: 'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'
+		};
+	});
